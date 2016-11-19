b317669fce3cbc5941140a77ba7dd6aa74ae1313
WeRockStar <vr_freestyle@outlook.com>
2016-11-19 14:05:16 +0700

    Refactor to constant buzz

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -15,22 +15,38 @@ public class FizzBuzzTest {
     @Test
     public void answer_given_1_return_1() throws Exception {
         String actual = fizzBuzz.answer(1);
-
         assertEquals("1", actual);
     }
 
     @Test
     public void answer_given_2_return_2() throws Exception {
         String actual = fizzBuzz.answer(2);
-
         assertEquals("2", actual);
     }
 
     @Test
     public void answer_given_3_return_Fizz() throws Exception {
-
         String actual = fizzBuzz.answer(3);
-
         assertEquals(FizzBuzz.FIZZ, actual);
     }
+
+    @Test
+    public void answer_given_9_return_Fizz() throws Exception {
+        String actual = fizzBuzz.answer(9);
+        assertEquals(FizzBuzz.FIZZ, actual);
+    }
+
+    @Test
+    public void answer_given_5_return_buzz() throws Exception {
+        String actual = fizzBuzz.answer(5);
+        assertEquals(FizzBuzz.BUZZ, actual);
+    }
+
+    @Test
+    public void answer_given_10_return_buzz() throws Exception {
+        String actual = fizzBuzz.answer(10);
+        assertEquals(FizzBuzz.BUZZ, actual);
+    }
+
+
 }
