f2239eb27b88ba89edf9ff40d1c3ae63efe592bc
WeRockStar <vr_freestyle@outlook.com>
2016-11-19 14:15:23 +0700

    Refactor to isBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -6,17 +6,21 @@ public class FizzBuzz {
     public static final String FIZZ_BUZZ = "FizzBuzz";
 
     public String answer(int number) {
-        if (isFizz(number) && number % 5 == 0)
+        if (isFizz(number) && isBuzz(number))
             return FIZZ_BUZZ;
 
         if (isFizz(number)) {
             return FIZZ;
         }
 
-        if (number % 5 == 0)
+        if (isBuzz(number))
             return BUZZ;
 
         return Integer.toString(number);
+    }
+
+    private boolean isBuzz(int number) {
+        return number % 5 == 0;
     }
 
     private boolean isFizz(int number) {
