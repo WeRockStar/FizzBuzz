9dc4d3a7bbbf993913c768b114ca83e4cf7c6e13
WeRockStar <vr_freestyle@outlook.com>
2016-11-19 13:50:32 +0700

    Refactor to constant

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,12 @@
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+
     public String answer(int number) {
+        if (number == 3) {
+            return FIZZ;
+        }
         return Integer.toString(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,13 +1,19 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void answer_given_1_return_1() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         String actual = fizzBuzz.answer(1);
 
         assertEquals("1", actual);
@@ -15,10 +21,16 @@ public class FizzBuzzTest {
 
     @Test
     public void answer_given_2_return_2() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         String actual = fizzBuzz.answer(2);
 
         assertEquals("2", actual);
+    }
+
+    @Test
+    public void answer_given_3_return_Fizz() throws Exception {
+
+        String actual = fizzBuzz.answer(3);
+
+        assertEquals(FizzBuzz.FIZZ, actual);
     }
 }
