cdb751e740cc84a04547d230e9fcbff7cd68ac12
WeRockStar <kotchaphan.m@gmail.com>
2017-07-19 10:24:29 +0700

    Update project

--- src/main/java/FizzBuzzRule.java
+++ src/main/java/FizzBuzzRule.java
@@ -2,7 +2,7 @@ public class FizzBuzzRule implements Rule {
 
     private final String FIZZ_BUZZ = "FizzBuzz";
     private Rule buzzRule = new BuzzRule();
-    private FizzRule fizzRule = new FizzRule();
+    private Rule fizzRule = new FizzRule();
 
     public boolean check(int number) {
         return buzzRule.check(number) && fizzRule.check(number);
